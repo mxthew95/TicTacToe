08f511489341cc6e5dcd3b5e3d46756567b68219
Mathew Ramish <mxthew95@gmail.com>
2022-02-01 19:47:55 +0800

    New repo

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Test from './Test';
+
+const App = () => {
+  return (
+    <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+      <Test />
+    </View>
+  )
+};
+
+export default App;
