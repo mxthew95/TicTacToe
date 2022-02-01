beec283bb82e6be722f92ccdd3121a054324df52
Mathew Ramish <mxthew95@gmail.com>
2022-02-01 19:59:40 +0800

    Removed Test.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import Test from './Test';
 
 const App = () => {
   return (
     <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
-      <Test />
+      <Text>Hello</Text>
     </View>
   )
 };
